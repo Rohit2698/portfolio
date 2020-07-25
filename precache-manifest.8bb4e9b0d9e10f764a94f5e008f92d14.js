a897208c4fe3bfb0d38a20be9d2d60e7bb4ff1b3
Rohit2698 <36307148+Rohit2698@users.noreply.github.com>
2020-07-25 13:24:27 +0530

    Updates

---
+++ precache-manifest.8bb4e9b0d9e10f764a94f5e008f92d14.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be828742bcf2dc921c5dfd551c8089c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc05ce6b71b27c593c68",
+    "url": "/portfolio/static/css/main.67f2dc2c.chunk.css"
+  },
+  {
+    "revision": "70283a13b67c5c8b8e06",
+    "url": "/portfolio/static/js/2.76308cb3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.76308cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc05ce6b71b27c593c68",
+    "url": "/portfolio/static/js/main.bd61ce1e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c6a84dfc69da8bb613cf138f386c787c",
+    "url": "/portfolio/static/media/covid.c6a84dfc.JPG"
+  },
+  {
+    "revision": "fdc3fd43f9e3babc8a99a23c5dc0b6c6",
+    "url": "/portfolio/static/media/head.fdc3fd43.jpg"
+  },
+  {
+    "revision": "03d5d55cc117b277c7d55d9980227cc4",
+    "url": "/portfolio/static/media/kbc.03d5d55c.JPG"
+  },
+  {
+    "revision": "1d12172cb5d6b781a8f8d8f7d592c9d7",
+    "url": "/portfolio/static/media/lyricistan.1d12172c.JPG"
+  },
+  {
+    "revision": "6f23ab74f3c5876491c4659a8a974eb9",
+    "url": "/portfolio/static/media/movie.6f23ab74.JPG"
+  },
+  {
+    "revision": "49772c429b82cd593cf06cbd38ba4177",
+    "url": "/portfolio/static/media/pokemon.49772c42.JPG"
+  },
+  {
+    "revision": "12aa77a646229e980e2062219a793689",
+    "url": "/portfolio/static/media/rohit.12aa77a6.jpg"
+  }
+]);
